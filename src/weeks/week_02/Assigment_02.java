969efa8c9b411acf575127018dd6cb829229b57d
soner <sonmezerdogan10@gmail.com>
2022-08-22 23:18:38 -0500

    added Assigment_02 file

--- src/weeks/week_02/Assigment_02.java
+++ src/weeks/week_02/Assigment_02.java
@@ -1,5 +1,8 @@
+package weeks.week_02;
+
 public class Assigment_02{
-	public class static void main(String[]args){
+	public static void main(String[]args) {
 		System.out.println("I love wooden spoon");
 	}
+
 }
