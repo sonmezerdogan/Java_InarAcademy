0b26a38635f07130e4086b294a7f85d64a6826e9
soner <sonmezerdogan10@gmail.com>
2022-08-24 08:24:10 -0500

    Exercise2_13 added

--- src/chapters/chapter_02/Exercise2_13.java
+++ src/chapters/chapter_02/Exercise2_13.java
@@ -1,10 +1,31 @@
 package chapters.chapter_02;
 
+import java.util.Scanner;
+
 public class Exercise2_13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the monthly saving amount: ");
+		
+		double m = input.nextDouble(); 
+		
+		double m1 = m * 1.00417;
+		double m2 = m1 * 1.00417;
+		double m3 = m2 * 1.00417;
+		double m4 = m3 * 1.00417;
+		double m5 = m4 * 1.00417;
+		double m6 = m5 * 1.00417;
+		double accountValue = m1 + m2 + m3 + m4 + m5 + m6;
+		
+		System.out.println("After six months The AccountValue is " + accountValue);
+		
+		
+		
+		
+		
 	}
 
 }
