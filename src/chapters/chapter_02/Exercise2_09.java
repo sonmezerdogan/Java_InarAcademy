695816aac2371286bcba5cd21c03abde09634791
soner <sonmezerdogan10@gmail.com>
2022-08-24 06:12:28 -0500

    Exercise2_09 changed

--- src/chapters/chapter_02/Exercise2_09.java
+++ src/chapters/chapter_02/Exercise2_09.java
@@ -1,10 +1,19 @@
 package chapters.chapter_02;
 
-public class Exercise2_09 {
-	
-	public static void main(String[]args) {
-		
-	
-	}
+import java.util.Scanner;
 
+public class Exercise2_09 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);	
+
+		System.out.print("Enter v0, v1 and t: ");
+		double v0 = input.nextDouble();
+		double v1 = input.nextDouble();
+		double t = input.nextDouble();
+		double a = (v1 - v0) / t;
+
+		System.out.println("The average acceleration is " + a);
+	}
 }
