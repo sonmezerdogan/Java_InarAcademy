4822b940cd5a93bbc15928f17bddb84c439a36ed
soner <sonmezerdogan10@gmail.com>
2022-08-25 15:46:34 -0500

    Two new files added

---
+++ src/chapters/chapter_03/MathRandom.java
@@ -0,0 +1,22 @@
+package chapters.chapter_03;
+
+public class MathRandom {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		
+	}
+
+}
