8da636b82527ed99d722f1f89ce723512767537b
soner <sonmezerdogan10@gmail.com>
2022-08-25 04:14:52 -0500

    Exercise_11 modified

--- src/chapters/chapter_01/Exercise_11.java
+++ src/chapters/chapter_01/Exercise_11.java
@@ -3,11 +3,38 @@ package chapters.chapter_01;
 public class Exercise_11 {
 
 	public static void main(String[] args) {
-
-		System.out.println("the population of The US after 5 years");
-		System.out.println(((5*365*24*60*60)/7)+((5*365*24*60*60)/45)-((5*365*24*60*60)/13)+312032486);
-		System.out.println("Calculation in a different way");
-		System.out.println((5*365*24*60*(60.0/7))+(5*365*24*60*(60.0/45))-(5*365*24*60*(60.0/13))+312032486);
+		
+		double secondInYear = 365 * 24 * 60 * 60;
+		
+		double currentPopulation = 312_032_486;
+		
+		double births = (365*24*60*60)/7;
+		
+		double immigrants = (365*24*60*60)/45;
+		
+		double deaths = (365*24*60*60)/13;
+		
+		currentPopulation = currentPopulation + births + immigrants - deaths;
+		
+		System.out.println("The population of first year is : " + (int)currentPopulation);
+		
+		currentPopulation = currentPopulation + births + immigrants - deaths;
+		
+		System.out.println("The population of second year is : " + (int)currentPopulation);
+		
+		currentPopulation = currentPopulation + births + immigrants - deaths;
+		
+		System.out.println("The population of third year is : " + (int)currentPopulation);
+		
+		currentPopulation = currentPopulation + births + immigrants - deaths;
+		
+		System.out.println("The population of forth year is : " + (int)currentPopulation);
+		
+		currentPopulation = currentPopulation + births + immigrants - deaths;
+		
+		System.out.println("The population of fifth year is : " + (int)currentPopulation);
+		
+		
 	}
 
 }
