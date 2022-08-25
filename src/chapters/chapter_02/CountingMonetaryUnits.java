711a4786941ce40e18d0ad027d36dae9beb511a2
soner <sonmezerdogan10@gmail.com>
2022-08-25 04:20:38 -0500

    Added CountingMonetaryUnits and Exercise2_15 files

---
+++ src/chapters/chapter_02/CountingMonetaryUnits.java
@@ -0,0 +1,41 @@
+package chapters.chapter_02;
+
+import java.util.Scanner;
+
+public class CountingMonetaryUnits {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter amount of money as decimal, such as 11.56: ");
+		
+		double amountMoney = input.nextDouble();
+		
+		double totalCents = amountMoney * 100;
+		
+		int dollars = (int) (totalCents / 100);
+		
+		double remainingCents = totalCents % 100;
+		
+		int quarters = (int) (remainingCents / 25);
+		
+		double remainingCents_1 = remainingCents % 25;
+		
+		int dimes = (int) (remainingCents_1 / 10);
+		
+		double remainingCents_2 = remainingCents_1 % 10;
+		
+		int nickels = (int) (remainingCents_2 / 5);
+		
+		double pennies = remainingCents_2 % 5;
+		
+		System.out.println("11.56 dollars consist of " + dollars + " dollars " + quarters + " quarters " +
+		 dimes + " dimes " + nickels + "nickels " + pennies + " pennies");
+		
+		
+		
+		
+	}
+
+}
---
+++ src/chapters/chapter_02/Exercise2_15.java
@@ -0,0 +1,8 @@
+package chapters.chapter_02;
+
+public class Exercise2_15 {
+	public static void main(String[]args) {
+		
+	}
+
+}
