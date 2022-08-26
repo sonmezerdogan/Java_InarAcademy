ef019b0d31d30f4465b173caa79a5536f7bc3a23
soner <sonmezerdogan10@gmail.com>
2022-08-26 08:05:57 -0500

    Exercise2_18 Math.pow file added

---
+++ src/chapters/chapter_02/Exercise2_18.java
@@ -0,0 +1,31 @@
+package chapters.chapter_02;
+
+public class Exercise2_18 {
+
+	public static void main(String[] args) {
+
+		float a, b;
+		System.out.println("a        b        pow(a, b)");
+		a = 1;
+		b = 2;
+		System.out.println((int)a + "        " + (int)b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println((int)a + "        " + (int)b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println((int)a + "        " + (int)b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println((int)a + "        " + (int)b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println((int)a + "        " + (int)b + 
+			"        " + (int)Math.pow(a, b));
+	}
+
+}
